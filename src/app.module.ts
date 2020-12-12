a44eb6f6f1875f7590ceea718ca3b5efa0826710
박준수 <pksjmh5295@gmail.com>
2020-12-12 13:00:45 +0900

    1.1 First Resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestModule } from './rest/rest.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot()],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile : join(process.cwd(),'src/schema.gql'),
+  }), RestModule],
   controllers: [],
   providers: [],
 })
