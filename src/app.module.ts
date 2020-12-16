8e3b533e2eeea974ebbd87f5169ede4a990d7808
박준수 <pksjmh5295@gmail.com>
2020-12-16 13:40:54 +0900

    2.3 Database, TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestModule } from './rest/rest.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 7777,
+      username: 'postgre',
+      password: 'pjsy',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
