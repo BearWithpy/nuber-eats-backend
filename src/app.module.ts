10018b59df3053085c1788130c41843fda83719c
박준수 <pksjmh5295@gmail.com>
2020-12-20 17:35:46 +0900

    2.6.1 Validating ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { RestModule } from './rest/rest.module';
 
 @Module({
@@ -40,24 +39,3 @@ import { RestModule } from './rest/rest.module';
   providers: [],
 })
 export class AppModule {}
-
-// What's Wrong with it
-
-// [Nest] 3416   - 2020. 12. 20. 오후 2:27:32   [InstanceLoader] GraphQLModule dependencies initialized +1ms
-// internal/crypto/keys.js:322
-//     throw new ERR_INVALID_ARG_TYPE(
-//     ^
-
-// TypeError [ERR_INVALID_ARG_TYPE]: The "key" argument must be of type string or an instance of Buffer, TypedArray, DataView, or KeyObject. Received null
-//     at prepareSecretKey (internal/crypto/keys.js:322:11)
-//     at new Hmac (internal/crypto/hash.js:111:9)
-//     at Object.createHmac (crypto.js:147:10)
-//     at createHMAC (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg\lib\sasl.js:133:17)
-//     at Hi (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg\lib\sasl.js:137:13)
-//     at Object.continueSession (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg\lib\sasl.js:32:24)
-//     at Client._handleAuthSASLContinue (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg\lib\client.js:257:10)
-//     at Connection.emit (events.js:315:20)
-//     at C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg\lib\connection.js:115:12
-//     at Parser.parse (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg-protocol\dist\parser.js:40:17) {
-//   code: 'ERR_INVALID_ARG_TYPE'
-// }
