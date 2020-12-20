251a8972cdaa39a1ba10687129e46ac0b83a5bbd
박준수 <pksjmh5295@gmail.com>
2020-12-20 12:15:20 +0900

    2.4 ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
@@ -6,13 +7,16 @@ import { RestModule } from './rest/rest.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 7777,
       username: 'postgres',
-      password: '12345', //real pw..............:(
-      //icannotbelieveit!
+      password: '12345',
       database: 'nuber-eats',
       synchronize: true,
       logging: true,
@@ -26,17 +30,3 @@ import { RestModule } from './rest/rest.module';
   providers: [],
 })
 export class AppModule {}
-
-//Error??????????????????????????????
-
-//[Nest] 24240   - 2020. 12. 16. 오후 1:42:46   [ExceptionHandler] ����� "postgres"�� password ������ �����߽��ϴ� +94ms
-// error: ����� "postgres"�� password ������ �����߽��ϴ�
-//     at Parser.parseErrorMessage (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg-protocol\dist\parser.js:278:15)
-//     at Parser.handlePacket (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg-protocol\dist\parser.js:126:29)
-//     at Parser.parse (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg-protocol\dist\parser.js:39:38)
-//     at Socket.<anonymous> (C:\Users\user\Desktop\nuber-eats-backend\node_modules\pg-protocol\dist\index.js:10:42)
-//     at Socket.emit (events.js:315:20)
-//     at addChunk (_stream_readable.js:309:12)
-//     at readableAddChunk (_stream_readable.js:284:9)
-//     at Socket.Readable.push (_stream_readable.js:223:10)
-//     at TCP.onStreamRead (internal/stream_base_commons.js:188:23)
