096d994257ae0da2f043d055da35540897914c67
박준수 <pksjmh5295@gmail.com>
2020-12-21 09:36:00 +0900

    3.0 First Entity

--- src/rest/entities/restaurant.entitiy.ts
+++ src/rest/entities/restaurant.entitiy.ts
@@ -1,19 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryColumn()
+  @Field((type) => Number)
+  id: number;
+
   @Field((type) => String)
+  @Column()
   name: string;
 
   @Field((type) => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field((type) => String)
+  @Column()
   address: string;
 
   @Field((type) => String)
-  ownersName: string;
+  @Column()
+  categoryName: string;
 
-  @Field((type) => Boolean, { nullable: true })
-  isGood?: boolean;
+  @Field((type) => String)
+  @Column()
+  ownersName: string;
 }
