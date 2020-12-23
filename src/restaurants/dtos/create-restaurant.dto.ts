500d79001adb46de493b2a2b60aed341fd06ff25
박준수 <pksjmh5295@gmail.com>
2020-12-23 09:49:59 +0900

    3.5.1 Mapped Types

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,10 +1,5 @@
-import { ArgsType, Field, InputType, OmitType } from '@nestjs/graphql';
-import { IsBoolean, IsString, Length } from 'class-validator';
+import { InputType, OmitType } from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurant.entitiy';
 
 @InputType()
-export class CreateRestaurantDto extends OmitType(
-  Restaurant,
-  ['id'],
-  InputType,
-) {}
+export class CreateRestaurantDto extends OmitType(Restaurant, ['id']) {}
