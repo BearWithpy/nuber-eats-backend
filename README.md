50676a3a1938d7ade776ac4b4f5e7d6d6cab04b9
박준수 <pksjmh5295@gmail.com>
2020-12-27 17:22:03 +0900

    .gitignore

---
+++ README.md
@@ -0,0 +1,22 @@
+# Nuber Eats (w. Nomadcoders)
+
+The Backend of Nuber Eats Clone
+
+## User Entity
+
+- id
+- createAt
+- updateAt
+
+- email
+- password
+- role(client | owner | delivery)
+
+## User CRUD
+
+- Create Account
+- Log In
+- See Profile
+
+- Edit Profile
+- Verify Email
---
+++ src/restaurants/entities/restaurant.entitiy.ts
@@ -0,0 +1,30 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@InputType({ isAbstract: true })
+@ObjectType()
+@Entity()
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  @Length(5)
+  name: string;
+
+  @Field((type) => Boolean, { nullable: false })
+  @Column({ default: false })
+  @IsOptional()
+  @IsBoolean()
+  isVegan: boolean;
+
+  @Field((type) => String, { defaultValue: 'Geoje' })
+  @Column()
+  @IsString()
+  address: string;
+}
+
