de2e99e2f7131abf7e1765512c5477f21f4ee02c
박준수 <pksjmh5295@gmail.com>
2021-01-07 13:18:17 +0900

    5.3.3 JWT Module : Edit

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,11 +12,8 @@ import { JwtService } from 'src/jwt/jwt.service';
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
-    private readonly config: ConfigService,
     private readonly jwtService: JwtService,
-  ) {
-    //this.jwtService.hello();
-  }
+  ) {}
 
   async createAccount({
     email,
@@ -58,7 +55,7 @@ export class UsersService {
           error: 'Wrong password',
         };
       }
-      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
+      const token = this.jwtService.sign(user.id);
       return {
         ok: true,
         token,
