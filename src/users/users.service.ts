a19e0b50f060fd788efa06d8f901c3f3c08aa808
박준수 <pksjmh5295@gmail.com>
2021-02-01 14:18:45 +0900

    7.10.1 editProfile : Test

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -31,7 +31,7 @@ export class UserService {
   }: CreateAccountInput): Promise<CreateAccountOutput> {
     try {
       const exists = await this.users.findOne({ email });
-      console.log(exists);
+      //console.log(exists);
       if (exists) {
         return { ok: false, error: 'There is a user with that email already' };
       }
