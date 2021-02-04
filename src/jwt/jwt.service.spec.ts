6fc2772ddffbbf9fdaa1d909a867f704bbdd9b9a
박준수 <pksjmh5295@gmail.com>
2021-02-04 09:15:47 +0900

    8.1 JWT Sign : Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
+import * as jwt from 'jsonwebtoken';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -21,6 +28,15 @@ describe('JwtService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  it.todo('sign');
+
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+      service.sign(ID);
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: 1 }, TEST_KEY);
+    });
+  });
+
   it.todo('verify');
 });
