a068e7c3cc8249b95556ce5069505d9a2cbd1fde
박준수 <pksjmh5295@gmail.com>
2021-02-16 09:42:45 +0900

    9.2 createAccount : E2E TEST

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-
+import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -16,15 +18,43 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
-  afterAll(async () => {
-    await getConnection().dropDatabase();
-    app.close();
-  });
+  describe('createAccount', () => {
+    const EMAIL = 'js@email.com';
+    const PASSWORD = '12345';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input: {
+            email:"${EMAIL}",
+            password:"${PASSWORD}",
+            role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
 
-  it.todo('createAccount');
+    it.todo('should fail if account already exists');
+  });
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
 });
