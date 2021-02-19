ecff6eb7e40e8971eae72994aaf72bc887bb2719
박준수 <pksjmh5295@gmail.com>
2021-02-19 09:38:15 +0900

    9.5 userProfile : E2E TEST

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,7 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { getConnection } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 jest.mock('got', () => {
   return {
@@ -12,8 +14,15 @@ jest.mock('got', () => {
 
 const GRAPHQL_ENDPOINT = '/graphql';
 
+const testUser = {
+  email: 'js@email.com',
+  password: '12345',
+};
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let usersRepository: Repository<User>;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,6 +30,7 @@ describe('UserModule (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
+    usersRepository = module.get<Repository<User>>(getRepositoryToken(User));
     await app.init();
   });
 
@@ -30,8 +40,6 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'js@email.com';
-    const PASSWORD = '12345';
     it('should create account', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
@@ -39,8 +47,8 @@ describe('UserModule (e2e)', () => {
           query: `
         mutation {
           createAccount(input: {
-            email:"${EMAIL}",
-            password:"${PASSWORD}",
+            email:"${testUser.email}",
+            password:"${testUser.password}",
             role:Owner
             }) {
               ok
@@ -63,8 +71,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation {
             createAccount(input: {
-              email:"${EMAIL}",
-              password:"12345",
+              email:"${testUser.email}",
+              password:"${testUser.password}",
               role:Owner
             }) {
               ok
@@ -83,8 +91,72 @@ describe('UserModule (e2e)', () => {
     });
   });
 
-  it.todo('userProfile');
-  it.todo('login');
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation{
+          login(input:{
+            email:"${testUser.email}",
+            password:"${testUser.password}",
+          }){
+            ok
+            error
+            token
+          }
+        }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+
+    it('should not be able to login with wrong credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation{
+          login(input:{
+            email:"${testUser.email}",
+            password:"$WrongPassword",
+          }){
+            ok
+            error
+            token
+          }
+        }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('Wrong password');
+          expect(login.token).toEqual(null);
+        });
+    });
+  });
+
+  describe('userProfile', () => {
+    beforeAll(async () => {
+      console.log(await usersRepository.find());
+      it("should see a user's profile", () => {});
+      it.todo('should not find a profile');
+    });
+  });
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
